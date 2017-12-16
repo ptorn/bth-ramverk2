5b55e96fe12834b006b2430c5f4b6cbf216bbc7e
Peder Tornberg <peder.tornberg@gmail.com>
2017-12-16 23:17:49 +0100

    Added data to projectpage

--- client/src/components/pages/Gomoku.js
+++ client/src/components/pages/Gomoku.js
@@ -18,6 +18,32 @@ export default class Gomoku extends Component {
                         Jag har börjat med att skapa en CI-kedja för att
                         löpande ha koll på min kod och dess kvalitet.
                     </p>
+
+                    <p>
+                        <h3>Gomoku Server</h3>
+                        <ul>
+                            <li>Express för att serva sidan och ett RESTapi för klienten</li>
+                            <li>MongoDB för att lagra data om spel och spelare.</li>
+                            <li>WebSockets för att hantera live-anslutningar</li>
+                            <li>bth-mongodb-crud vilket är min modul för att
+                                hantera CRUD. Modulen finns
+                                <a
+                                    href="https://www.npmjs.com/package/bth-mongodb-crud"
+                                    title="bth-mongodb-crud"
+                                >
+                                    NPM bth-mongodb-crud
+                                </a>
+                            </li>
+                        </ul>
+                        <h3>Gomoku Klient</h3>
+                        <ul>
+                            <li>React</li>
+                            <li>WebSockets för kommunikation mot RESTful servern.</li>
+                            <li>Chatmodulen som vi skrev tidigare för att chatta
+                                medans man spelar.
+                            </li>
+                        </ul>
+                    </p>
                     <p>
                         Ni kan hitta applikationen på github genom att
                         <a
